efc58da10737af02bfd26623904bee98ef321b57
Arpit000009 <arpitpandey000009@gmail.com>
2026-06-27 14:54:57 +0530

    storing chat history in DB

--- src/main/java/com/substring/helpdesk/config/AiConfig.java
+++ src/main/java/com/substring/helpdesk/config/AiConfig.java
@@ -4,6 +4,8 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,7 +18,14 @@ public class AiConfig {
     private Logger logger = org.slf4j.LoggerFactory.getLogger(AiConfig.class);
 
     @Bean
-    public ChatClient chatClient(ChatClient.Builder builder, ChatMemory chatMemory){
+    public ChatClient chatClient(ChatClient.Builder builder, JdbcChatMemoryRepository jdbcChatMemoryRepository){
+
+        //chat memory ko create kar sakte hain
+
+        var chatMemory = MessageWindowChatMemory.builder()
+                        .chatMemoryRepository(jdbcChatMemoryRepository)
+                        .maxMessages(15)
+                                .build();
 
         logger.info("chatClient bean created");
         logger.info("chat memery bean created. {}",chatMemory.getClass().getName());
